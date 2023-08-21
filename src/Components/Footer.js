0fd0cfb1f089305fa0dfff9613be9f3588c368d9
emrgcmnn <e.gocmen1993@gmail.com>
2023-08-21 23:35:49 +0300

    kampanya

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -15,23 +15,32 @@ const Footer = () =>  {
     return (
      
         <div class="text-center">
-        <h1 class="text-3xl font-bold mb-4">İletişim</h1>
+        <h1 class="text-3xl font-bold m-4">İLETİŞİM</h1>
+        
         <div class="flex justify-center">
+            
             <div class="w-1/4 text-center">
-                <img src={Facesvg} alt="Facebook" class="mx-auto mb-2 w-8 h-8"/>
-                <p class="font-semibold">Facebook</p>
+                <button>
+                    <img src={Facesvg} alt="Facebook" class="mx-auto mb-2 w-8 h-8"/>
+                </button>
             </div>
+
             <div class="w-1/4 text-center">
-                <img src={Twitsvg} alt="Twitter" class="mx-auto mb-2 w-8 h-8"/>
-                <p class="font-semibold">Twitter</p>
+                <button>
+                    <img src={Twitsvg} alt="Twitter" class="mx-auto mb-2 w-8 h-8"/>
+                </button>
             </div>
+            
             <div class="w-1/4 text-center">
-                <img src={Whatsappsvg} alt="WhatsApp" class="mx-auto mb-2 w-8 h-8"/>
-                <p class="font-semibold">WhatsApp</p>
+                <button>
+                    <img src={Whatsappsvg} alt="WhatsApp" class="mx-auto mb-2 w-8 h-8"/>
+                </button>
             </div>
+            
             <div class="w-1/4 text-center">
-                <img src={Inssvg} alt="Instagram" class="mx-auto mb-2 w-8 h-8"/>
-                <p class="font-semibold">Instagram</p>
+                <button>
+                    <img src={Inssvg} alt="Instagram" class="mx-auto mb-2 w-8 h-8"/>
+                </button>
             </div>
         </div>
     </div>
