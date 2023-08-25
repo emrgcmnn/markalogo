f25fd6942fa50fa23ec4c312c404ef4a5c429c71
emrgcmnn <e.gocmen1993@gmail.com>
2023-08-25 18:16:56 +0300

    gereksiz dosyalar eklendi

--- src/App.js
+++ src/App.js
@@ -3,14 +3,10 @@ import Navbar from '../src/Components/Navbar';
 import UnderMenu from './Components/UnderMenu';
 import NasilSiparis from './Components/NasilSiparis'
 import Bizkimiz from './Components/BizKimiz'
-import Footer from './Components/Footer'
 import Kampanyalar from './Components/Urunler'
 import Form from './Components/Form'
-import { Routes, Route,Switch , Link, NavLink } from 'react-router-dom';
 
-import BizKimiz from '../src/pages/BizKimiz'
-import Iletisim from '../src/pages/Iletisim'
-import Urunler from '../src/pages/Urunler'
+
 
 
 function App() {
@@ -37,15 +33,7 @@ function App() {
 
 
 
-      
-       {/* <Routes>
-            
-  
-          <Route path="/AnaSayfa" element = {<AnaSayfa />} />
-          <Route path="/BizKimiz" element = {<BizKimiz />} />
-          <Route path="/Iletisim" element = {<Iletisim />} />
-          <Route path="/Urunler" element = {<Urunler />} />
-        </Routes>  */}
+     
        </>
     
   );
