8d30383ba415698cc2492314c4ca7da913ffe86b
emrgcmnn <e.gocmen1993@gmail.com>
2023-08-28 23:50:35 +0300

    referanslar kısmı hazırlanıyor

---
+++ src/Components/Refarans.js
@@ -0,0 +1,38 @@
+import React, { useState , useEffect, useRef} from 'react'
+
+import AtesPilic from '../img/ates.jpeg'
+import AtesPilic2 from '../img/ates2.jpeg'
+import Isparta from '../img/isparta.jpeg'
+import Kahve from '../img/kahve.jpeg'
+
+const Referans = () => {
+    
+    return (
+        <>
+            <div>
+                <div>REFERANSLARIMIZ</div>
+                
+                <div>
+                    <div>
+                        <img src={AtesPilic} ></img>
+                    </div>
+                    <div>
+                        <img src={AtesPilic2} ></img>
+                    </div>
+                </div>
+
+                <div>
+                    <div>
+                        <img src={Isparta} ></img>
+                    </div>
+                    <div>
+                        <img src={Kahve} ></img>
+                    </div>
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default Referans;
